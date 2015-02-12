5e2992f7e0ea25759a13ba7d742e946cb2664f92
Pirial <ignatovimc@live.com>
2015-02-12 22:03:37 +0200

    2

--- Patterns/WpfApplication2/CreatePatterns/AbstractFactory.cs
+++ Patterns/WpfApplication2/CreatePatterns/AbstractFactory.cs
@@ -27,10 +27,6 @@ class MainApp
         Client c2 = new Client(factory2);
         c2.Run();
     }
-
-
-
-
     // Wait for user input Console.Read();
     // "AbstractFactory"
     abstract class AbstractFactory
